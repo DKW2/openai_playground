4ed55e836018aef2056f2f6e2eacb84768f7a215
DKW2 <derek.kiah.wang@gmail.com>
2023-07-20 18:54:37 -0700

    Fixed Reset command

--- playground.py
+++ playground.py
@@ -2,7 +2,7 @@ import os
 import openai
 
 openai.organization = "org-4JqLNzk5F86VGcrPxDKwcFZl"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key_path = "/home/cupsponge/projects/keys/openai.key"
 #print( openai.Model.list() )
 
 MODEL = "gpt-3.5-turbo"
@@ -15,16 +15,17 @@ userMsg = input( "What would you like to ask chatGPT? Type 'Reset' to reset the 
 while( userMsg != "Quit" ):
     if( userMsg == "Reset" ):
         messages = [{"role": "system", "content": "You are a helpful assistant."}]
-    messages.append( {"role": "user", "content": userMsg } )
-    response = openai.ChatCompletion.create(
-        model=MODEL,
-        messages=messages
-    )
+    else:
+        messages.append( {"role": "user", "content": userMsg } )
+        response = openai.ChatCompletion.create(
+            model=MODEL,
+            messages=messages
+        )
 
-    aiMsg = response["choices"][0]["message"]
-    print( "\n" + aiMsg["content"] )
-    print( "(Total tokens: " + str( response["usage"]["total_tokens"] ) + ")" )
-    messages.append( aiMsg )
+        aiMsg = response["choices"][0]["message"]
+        print( "\n" + aiMsg["content"] )
+        print( "(Total tokens: " + str( response["usage"]["total_tokens"] ) + ")" )
+        messages.append( aiMsg )
 
     userMsg = input( "\nWhat would you like to ask chatGPT? Type 'Reset' to reset the conversation, or 'Quit' to quit the program.\n" )
 
